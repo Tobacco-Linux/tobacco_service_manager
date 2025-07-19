45473e055a8f3ef289b126583be464eac6f6f2d2
dehrexd <dehrexd@gmail.com>
2025-07-20 06:20:16 +0800

    horizontal paning

--- main.c
+++ main.c
@@ -25,15 +25,31 @@ static gboolean populate_store(gpointer data) {
 static void activate(GtkApplication *app, gpointer user_data) {
   (void)user_data;
   GtkWidget *window = gtk_application_window_new(app);
+  GtkWidget *horizontal_panel = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
+  GtkWidget *sidebar = gtk_frame_new(NULL);
+  GtkWidget *list_view;
+
   gtk_window_set_title(GTK_WINDOW(window), "Service Managers");
-  gtk_window_set_default_size(GTK_WINDOW(window), 400, 600);
+  gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+  gtk_widget_set_size_request(horizontal_panel, 200, -1);
 
   AppData *data = g_new0(AppData, 1);
   data->store = g_list_store_new(GTK_TYPE_STRING_OBJECT);
 
-  GtkWidget *list_view = service_list_new(data->store);
-  gtk_window_set_child(GTK_WINDOW(window), list_view);
+  list_view = service_list_new(data->store);
 
+  gtk_paned_set_start_child(GTK_PANED(horizontal_panel), sidebar);
+  gtk_paned_set_resize_start_child(GTK_PANED(horizontal_panel), TRUE);
+  gtk_paned_set_shrink_start_child(GTK_PANED(horizontal_panel), FALSE);
+  gtk_widget_set_size_request(sidebar, 250, -1);
+
+  gtk_paned_set_end_child(GTK_PANED(horizontal_panel), list_view);
+  gtk_paned_set_resize_end_child(GTK_PANED(horizontal_panel), FALSE);
+  gtk_paned_set_shrink_end_child(GTK_PANED(horizontal_panel), FALSE);
+  gtk_widget_set_size_request(list_view, 550, -1);
+
+  gtk_window_set_child(GTK_WINDOW(window), horizontal_panel);
   g_idle_add(populate_store, data);
   gtk_window_present(GTK_WINDOW(window));
 }
